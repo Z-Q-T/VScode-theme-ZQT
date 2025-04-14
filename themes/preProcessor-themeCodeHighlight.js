1c6e7fe7ab9abcc1e4895e6bc44b0d45913e4f06
Zhang Qingtian <im_zhangqingtian@163.com>
2025-04-15 01:27:59 +0800

    更新内容

--- themes/preProcessor-themeCodeHighlight.js
+++ themes/preProcessor-themeCodeHighlight.js
@@ -24,7 +24,7 @@ let GREY95 = "#eeeeee";
 let WHITE = "#FFF";
 
 let errorStrong = "#ff3b3b";
-
+//——————以下注释作废——————//
 //H220和H240比较接近蓝色，尽量不用
 //避免使用大红大橙，以防和报错信息混淆
 //内置：红，340、0、20、40
@@ -32,6 +32,13 @@ let errorStrong = "#ff3b3b";
 //自写-不可复用（数值、字符串）：绿，140、160、180、200
 //蓝，220、240、260
 //紫，280、300、320
+//——————以上注释作废——————//
+
+//TODO
+//自写-不可复用：红橙，330-80
+//自写-可复用：黄绿，90-200
+//内置：蓝，210-320
+
 import colour from "./color_palette.js";
 
 import fs from "fs";
@@ -52,33 +59,37 @@ function generateTokenColors(rules) {
 	};
 }
 
+let entityCharacterColour = colour.L72_C12_H160;
+let commentColour = GREY50;
+
 // 定义规则数据
 const colorRules = [
 	// { scopes: ["namespace", "class"], color: WHITE },//多个写在一起的写法
-	{ scopes: "comment", color: GREY50 },
+	{ scopes: "comment", color: commentColour },
 	{ scopes: "invalid", color: errorStrong },
-	{ scopes: "constant.numeric", color: colour.L72_C12_H200 }, //自写-不可复用
-	{ scopes: "constant.character", color: colour.L72_C12_H160 }, //内置
+	{ scopes: "constant.numeric", color: colour.L72_C12_H180 }, //自写-不可复用
+	{ scopes: "constant.character", color: entityCharacterColour }, //内置
 	{ scopes: "constant.language", color: colour.L72_C14_H000 }, //内置
 	{ scopes: "entity", color: colour.L72_C14_H060 }, //自写-可复用
-	{ scopes: "entity.name.function", color: colour.L72_C14_H060 }, //自写-可复用
-	{ scopes: "entity.name.tag", color: colour.L72_C14_H060 }, //内置
-	{ scopes: "entity.other.attribute-name", color: colour.L72_C14_H060 }, //内置
+	{ scopes: "entity.name.function", color: colour.L72_C14_H070 }, //自写-可复用
+	{ scopes: "entity.name.tag", color: colour.L72_C10_H350 }, //内置
+	{ scopes: "entity.other.attribute-name", color: colour.L72_C08_H070 }, //内置
 	{ scopes: "keyword", color: colour.L72_C14_H020 }, //内置
-	{ scopes: "keyword.control", color: colour.L72_C14_H020 }, //内置
-	{ scopes: "keyword.operator", color: colour.L72_C14_H040 }, //内置
-	{ scopes: "meta", color: colour.L72_C12_H240 },//元
-	{ scopes: "punctuation", color: GREY80 },//元
-	{ scopes: "punctuation.definition.comment", color: GREY50 },//元
+	{ scopes: "keyword.control", color: colour.L72_C12_H030 }, //内置
+	{ scopes: "keyword.operator", color: colour.L74_C14_H010 }, //内置
+	{ scopes: "meta", color: colour.L72_C08_H230 }, //元
+	{ scopes: "punctuation", color: colour.L72_C06_H250 }, //元
+	{ scopes: "punctuation.definition.comment", color: commentColour }, //元
+	{ scopes: "punctuation.definition.entity", color: entityCharacterColour }, //元
 	{ scopes: "storage", color: colour.L72_C10_H340 }, //内置
 	{ scopes: "string", color: colour.L72_C08_H160 }, //自写-不可复用
-	{ scopes: "string.regexp", color: WHITE },//半自写
-	{ scopes: "string.interpolated", color: colour.L72_C14_H160 },//半自写
+	{ scopes: "string.regexp", color: colour.L74_C14_H310 }, //半自写
+	{ scopes: "string.interpolated", color: colour.L72_C14_H290 }, //半自写
 	{ scopes: "support", color: colour.L72_C14_H000 }, //内置
 	{ scopes: "support.function", color: colour.L72_C14_H000 }, //内置
-	{ scopes: "variable", color: colour.L72_C08_H070 }, //自写-可复用
-	{ scopes: "variable.parameter", color: colour.L72_C08_H080 }, //自写-可复用
-	{ scopes: "variable.language", color: colour.L72_C08_H080 }, //内置
+	{ scopes: "variable", color: colour.L72_C08_H080 }, //自写-可复用
+	{ scopes: "variable.parameter", color: colour.L72_C08_H100 }, //自写-可复用
+	{ scopes: "variable.language", color: colour.L72_C08_H10 }, //内置
 ];
 
 // 生成JSON对象
